02e39f5caeffc556dfe785cd5edc00c6e61bafe3
Emiliano Labarthe <emilianolabarthe123@gmail.com>
2023-11-01 18:23:54 -0300

    some routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelectProposalComponent } from './select-proposal/select-proposal.component';
+import { CreateProposalComponent } from './create-proposal/create-proposal.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'select-proposal', component: SelectProposalComponent },
+  { path: 'create-proposal', component: CreateProposalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
