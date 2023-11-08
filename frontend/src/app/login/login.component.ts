580cf828ef5d68781f691d4bcd501903644c9ff5
tomasdarracq <tomasdarracq12@gmail.com>
2023-11-08 10:23:56 -0300

    admin pronto SIN HASHEAR

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AdminServiceService } from '../admin-service.service';
+import { Router } from '@angular/router';
+import { Admin } from '../admin';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private adminService: AdminServiceService, private router: Router) { }
+  admin: Admin = {
+    name: "",
+    password: ""
+  }
+  errorMessage: string = "";
+  sendAdmin() {
+    this.adminService.postAdmin(this.admin).subscribe(
+      (response: any) => {
+        if (response.exists == true) {
+          console.log('Admin successfully sent:', response);
+          this.router.navigate(['/select-proposal']);
+        } else {
+          console.log('Admin not found');
+          this.errorMessage = 'Incorrect username or password';
+        }
+      },
+      (error) => {
+        console.log('Error sending proposal:', error);
+        this.errorMessage = 'Server error occurred';
+        // Handle the error (show a message to the user, etc.)
+      }
+    );
+  }
+
+
 
 }
