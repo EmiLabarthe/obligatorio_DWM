e24ddbb8c8e7221760c54b4d648c6d3631cb7d97
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2023-11-13 10:55:10 -0300

    beartoken

--- backend/middleware/jwtAuth.js
+++ backend/middleware/jwtAuth.js
@@ -2,18 +2,23 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 function generateToken(payload) {
-  return jwt.sign({algo:payload},"shhh", { expiresIn: '1h' }); 
+  console.log(" process.env.JWT_SECRET; ",  process.env.JWT_SECRET)
+  return jwt.sign({algo:payload}, process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: '1h' }); 
 }
 
+
+
 function verifyToken(req, res, next) {
-  const token = req.header('Authorization');
+  const token = req.headers.authorization?.split(" ")[1];
     console.log("ACAESTOY");
+    console.log(token);
   if (!token) {
     return res.status(401).json({ message: 'Acceso no autorizado' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
+      console.log("err: " , err )
       return res.status(403).json({ message: 'Token no válido' });
     }
     req.user = user;
