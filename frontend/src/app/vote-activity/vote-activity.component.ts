ecb98ded46f8fa6503b3c6c9945a87fbee1d45e9
ManuelM512 <manuel5mongelos@gmail.com>
2023-11-14 08:39:12 -0300

    Integrated activities

--- frontend/src/app/vote-activity/vote-activity.component.ts
+++ frontend/src/app/vote-activity/vote-activity.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Activity } from '../activity';
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-vote-activity',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./vote-activity.component.css']
 })
 export class VoteActivityComponent {
-
+  currentActivity:Activity = {title:'Esperando actividad', imgPath:''};
+  voto: boolean = false;
+  constructor(private socketService: SocketService) {}
+  activityPosition = -1;
+  nickname = localStorage.getItem('nickname');
+  
+  onClick(reaction: number)
+  {
+    this.voto = false;
+    this.nickname;
+  }
+  ngOnInit() {
+    //chart.js
+    this.socketService.getNewMessage().subscribe((activity:Activity) => {
+      this.currentActivity = activity;
+      this.voto = activity.title != "Terminó el juego" && activity.title != "Esperando actividad";
+      this.activityPosition += 1;
+    })
+  }
 }
