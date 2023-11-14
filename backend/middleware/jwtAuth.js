dbcdf5d8533ad89f92b7b711b1514872d79ea0eb
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2023-11-14 10:59:14 -0300

    a

--- backend/middleware/jwtAuth.js
+++ backend/middleware/jwtAuth.js
@@ -9,7 +9,7 @@ function generateToken(payload) {
 
 
 function verifyToken(req, res, next) {
-  
+  //cwadwa
   //const token = req.headers.authorization?.split(" ")[1];
   const token = req.headers.authorization;
     console.log("ACAESTOY");
