37a2fd5c11964300b7a5ae117dad47a18f045c2e
Emiliano Labarthe <emilianolabarthe123@gmail.com>
2023-11-27 10:51:23 -0300

    socket connected to guest

--- frontend/src/app/guest-wait/guest-wait.component.ts
+++ frontend/src/app/guest-wait/guest-wait.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../socket.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Activity } from '../activity';
 
 @Component({
   selector: 'app-guest-wait',
@@ -7,9 +10,33 @@ import { Component } from '@angular/core';
 })
 export class GuestWaitComponent {
 
-  nickname?: string | null;
+    currentActivity:Activity = {title:'Esperando actividad', imgPath:''};
+    voto: boolean = false;
+    sessionId: string = "";
+    constructor(private socketService: SocketService, private router: Router, private route: ActivatedRoute) {}
+    activityPosition = -1;
+  
+    nickname?: string | null;
 
-  ngOnInit(): void {
-    this.nickname = localStorage.getItem('nickname');
+    ngOnInit(): void {
+      this.nickname = localStorage.getItem('nickname');
+      this.route.params.subscribe(params => {
+        this.sessionId = params['sessionId'];    
+        
+        this.socketService.getNewMessage().subscribe((message) => {
+          if(message.activity.title != "Esperando actividad"){
+            this.router.navigate([`/vote-activity/${this.sessionId}`])
+          }
+          this.currentActivity = message.activity;
+          this.activityPosition = message.position;
+          this.voto = this.activityPosition !=-1;
+          if(this.currentActivity.title == "Terminó el juego" && this.activityPosition == -1)
+          {
+            this.router.navigate([`/ranking-proposal/${this.sessionId}`])
+          }
+      })
+      }); 
+
+      
   }
 }
